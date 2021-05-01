10adc63c639401709aec1b543c4480d4cf9c4dc3
Zuzana Franova <franova.z@gmail.com>
2021-05-01 12:08:34 +0200

    Order button script

--- src/index.js
+++ src/index.js
@@ -6,11 +6,32 @@ console.log('funguju!');
 
 const navBtnElm = document.querySelector('#nav-btn');
 const navElm = document.querySelector('nav');
+const navLinksElm = document.querySelectorAll('nav a');
 
 navBtnElm.addEventListener('click', () => {
   navElm.classList.toggle('nav-closed');
 });
 
-navElm.addEventListener('click', () => {
-  navElm.classList.add('nav-closed');
+navLinksElm.forEach((elm) => {
+  elm.addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+});
+
+// Objednavani napoje po clicknuti na Objednej btn
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinkCupElm = document.querySelector('.drink__cup');
+let ordered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (ordered === false) {
+    orderBtnElm.textContent = 'Zrušit';
+    drinkCupElm.classList.add('drink__cup--selected');
+    ordered = true;
+  } else if (ordered === true) {
+    orderBtnElm.textContent = 'Objednat';
+    drinkCupElm.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
 });
