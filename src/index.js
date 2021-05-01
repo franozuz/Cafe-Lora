a6b758b061b71f4c24cd9badfb3ea0ca516bf6a2
Zuzana Franova <franova.z@gmail.com>
2021-05-01 11:35:15 +0200

    Hamburger menu dropdown set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+// Hamburger menu will show dropdown menu on click and hide when a navigation link is clicked.
+
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+navElm.addEventListener('click', () => {
+  navElm.classList.add('nav-closed');
+});
